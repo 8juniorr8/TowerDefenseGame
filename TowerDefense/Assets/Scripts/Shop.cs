1c0fa039a32c586153ff8db92da7ea4e04d74043
8juniorr8 <stleharm@stud.h-da.de>
2019-07-16 01:10:14 +0200

    Shop added

---
+++ TowerDefense/Assets/Scripts/Shop.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class Shop : MonoBehaviour
+{
+
+    BuildManager buildManager;
+
+    void Start ()
+    {
+        buildManager = BuildManager.instance;
+    }
+
+
+    public void PurchaseStandardTurret ()
+    {
+        Debug.Log("Standard Turret Selected");
+        buildManager.SetTurretToBuild(buildManager.standardTurretPrefab);
+    }
+
+    public void PurchaseSecondTurret()
+    {
+        Debug.Log("Second Turret Selected");
+        buildManager.SetTurretToBuild(buildManager.secondTurretPrefab);
+    }
+}
