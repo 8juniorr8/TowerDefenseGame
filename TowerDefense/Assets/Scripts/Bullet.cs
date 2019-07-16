9b316df47571e6c249c6f7e9cf1631f4fb218da8
8juniorr8 <stleharm@stud.h-da.de>
2019-07-17 00:31:42 +0200

    Added Lives, Added Health (Enemy), Updated Effects

--- TowerDefense/Assets/Scripts/Bullet.cs
+++ TowerDefense/Assets/Scripts/Bullet.cs
@@ -5,6 +5,7 @@ public class Bullet : MonoBehaviour
     private Transform target;
 
     public float speed = 70f;
+    public int damage = 20;
     public float explosionRadius = 0f;
     public GameObject impactEffect;
 
@@ -71,7 +72,13 @@ public class Bullet : MonoBehaviour
 
     void Damage (Transform enemy)
     {
-        Destroy(enemy.gameObject);
+        Enemy e = enemy.GetComponent<Enemy>();
+
+        if (e != null)
+        {
+            e.TakeDamage(damage);
+        }
+
     }
 
     void OnDrawGizmosSelected ()
