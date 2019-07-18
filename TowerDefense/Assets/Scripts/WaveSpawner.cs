04b99abc7015557a6f1157984a2e009451cc0142
8juniorr8 <stleharm@stud.h-da.de>
2019-07-18 17:38:23 +0200

    Balancing Changes

--- TowerDefense/Assets/Scripts/WaveSpawner.cs
+++ TowerDefense/Assets/Scripts/WaveSpawner.cs
@@ -10,7 +10,7 @@ public class WaveSpawner : MonoBehaviour{
 
 
     public float timeBetweenWaves = 5f;
-    private float countdown = 2f;
+    private float countdown = 10f;
 
     public Text waveCountdownText;
 
